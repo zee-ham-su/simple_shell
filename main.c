ad9a200bd674a378a53165c7d1a4dbc65f949c5e
Gideon-Gyebi <gyebigideon000@gmail.com>
2023-05-25 01:38:27 +0000

    psuh to run checker of main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include "shell.h"
+/**
+ * main - Simple shell; prints the prompt with the shell
+ * name.
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char **env = NULL;
+	char *shellName = "MyShell";
+
+	return (0);
+}
